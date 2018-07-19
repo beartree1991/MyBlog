c6b747de95b689583bc5c0f01bf6865b898a37db
beartree1991 <beartree1991@163.com>
2018-07-19 19:54:45 +0800

    20180719

---
+++ www/coroweb.py
@@ -0,0 +1,17 @@
+'''
+ * @Author: beartree 
+ * @Date: 2018-07-12 22:11:50 
+ * @Last Modified by:   beartree 
+ * @Last Modified time: 2018-07-12 22:11:50 
+'''
+
+import asyncio, os, inspect, logging, functions
+from urllib import parse
+from aiohttp import web
+from apis import APIError
+
+def get(path):
+    '''
+    Define decorator @get('/path')
+    '''
+    def decorator(func):

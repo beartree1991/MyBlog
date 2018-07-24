0b0961f22d471c7e6b3fc82d3ac52f878524def8
beartree1991 <beartree1991@163.com>
2018-07-24 22:20:55 +0800

    20180724

--- www/coroweb.py
+++ www/coroweb.py
@@ -5,7 +5,7 @@
  * @Last Modified time: 2018-07-12 22:11:50 
 '''
 
-import asyncio, os, inspect, logging, functions
+import asyncio, os, inspect, logging, functools
 from urllib import parse
 from aiohttp import web
 from apis import APIError
@@ -14,3 +14,25 @@ def get(path):
     '''
     Define decorator @get('/path')
     '''
+    def decorator(func):
+        @functools.wraps(func) # 装饰器等效于 func = functools.wraps(func)
+        def wrapper(*args, **kw)
+            return func(*args, **kw)
+        wrapper.__method__ = 'GET'
+        wrapper.__route__ = path
+        return wrapper
+    return decorator
+
+def post(path):
+    '''
+    Define decorator @post('/path')
+    '''
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kw):
+            return func(*args, **kw)
+        wrapper.__method__ = 'POST'
+        wrapper.__route__ = path
+        return wrapper
+    return decorator
+
